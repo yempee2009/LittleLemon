f2bbd3b3029bf171ef114763c2a5ffde07593c05
Mahesh <yempee2009@gmail.com>
2026-06-27 12:30:18 +0530

    did the database changes

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -73,11 +73,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'littlelemon',  
+        'USER': 'root',  
+        'PASSWORD': 'root',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, Booking
+
+admin.site.register(Menu)
+admin.site.register(Booking)
