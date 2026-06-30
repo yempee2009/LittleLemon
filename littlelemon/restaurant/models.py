52796c28a6db193e5c5d0ba45e30b65ebf3b8aac
Mahesh <yempee2009@gmail.com>
2026-06-30 22:55:43 +0530

    unit test added

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -6,8 +6,10 @@ class Menu(models.Model):
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
 
+    
+
     def __str__(self):
-        return self.Title
+        return f'{self.title} : {str(self.price)}'
 
 
 class Booking(models.Model):
